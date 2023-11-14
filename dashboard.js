206a64035f34ccc0c2e40b803edc1c1ce784f895
Benedicta Djangmah <benedicta.djangmah@amalitech.com>
2023-11-14 13:05:52 +0100

    first commit

---
+++ dashboard.js
@@ -0,0 +1,25 @@
+//  for the logout button 
+document.addEventListener("DOMContentLoaded", function () {
+    // Code inside this block will run when the DOM is fully loaded
+    // This ensures that the JavaScript code doesn't run until the HTML document is ready
+    const logoutButton = document.querySelector(".logout button");
+  
+    logoutButton.addEventListener("click", function () {
+      // Assuming you want to redirect to a login page after logout
+      window.location.href = "login.html";
+    });
+  });
+  
+  ;
+  // for the profile 
+  document.addEventListener("DOMContentLoaded", function () {
+    const profileImage = document.querySelector(".profileInfo");
+  
+    profileInfo.addEventListener("click", function () {
+     
+      window.location.href = "profile.html";
+    });
+  });
+  
+
+  
